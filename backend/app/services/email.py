93592569f686f6386f70767a3e050d1fbca3ae96
Nicolas Le Tertre <nicolas.letertre35@gmail.com>
2024-02-06 22:58:49 +0100

    Refactor holehe usage

--- backend/app/services/email.py
+++ backend/app/services/email.py
@@ -1,24 +1,10 @@
-import pprint
-import subprocess
-import os
-import pandas as pd
+import trio
+from app.utils.light_holehe import run_light_holehe
 
 def get_holehe(email):
     domain_values = []
-    command = f"holehe --csv {email}"
     try:
-        result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        # Attend que la commande se finisse
-        result.communicate()
-
-        files = [file for file in os.listdir('.') if file.startswith('holehe_') and file.endswith('.csv')]
-        if files:
-            file_path = files[0]
-            data = pd.read_csv(file_path)
-            filtered_data = data[data['exists'] == True]
-            domain_values = filtered_data['domain'].tolist()
-            os.remove(file_path)
+        domain_values = trio.run(run_light_holehe, email)
     except Exception as e:
         pass
     return domain_values

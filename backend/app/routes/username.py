cac059ddad2b4a7abb86dd66e86383a52ff1093c
mathysv <90249274+mathysv@users.noreply.github.com>
2024-02-15 19:29:15 +0100

    Première version de l'intégration de black bird

---
+++ backend/app/routes/username.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, abort, request, jsonify
+from app.services.username import get_blackbird
+
+username = Blueprint('username', __name__)
+
+@username.route('api/username',methods=['GET'])
+def get_username() :
+    username = request.args.get('value')
+
+    if username is None :
+        abort(400)
+
+    userJson = get_blackbird(username)
+    return jsonify(userJson)
+
+@username.errorhandler(400)
+def bad_request(error):
+    return jsonify({"error": "Bad Request, please ensure all parameters are provided"}), 400

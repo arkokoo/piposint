a673086bbf844839df62253dee5699dcf087dc4b
Mathysv <mathysvsbe@gmail.com>
2024-02-27 18:48:25 +0100

    ajout de l'api ip

---
+++ backend/app/services/ip.py
@@ -0,0 +1,12 @@
+from app.utils.Requester import Requester
+from app.utils.tools import get_country, get_countries
+
+def get_ip(search_value) :
+    json_data = {}
+    url = f"http://ip-api.com/json/{search_value}?fields=20474"
+    response = Requester(url=url).get()
+    if response and response.status_code == 200 :
+        countries = get_countries()
+        json_data = response.json()
+        json_data['country'],json_data['country_flag'] = get_country(countries,json_data['countryCode'])
+        return json_data

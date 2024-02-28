a970c83d617be75abd06068b7fb3831f2e071481
Mathysv <mathysvsbe@gmail.com>
2024-02-28 09:18:27 +0100

    intégration fonction country + erreur sur les values

--- backend/app/routes/ip.py
+++ backend/app/routes/ip.py
@@ -1,10 +1,6 @@
 from flask import Blueprint, abort, request, jsonify
-import re
+from app.utils.ip.ip_regex import regex_compare
 from app.services.ip import get_ip
- 
-REGEX_IPV4 = r'^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?<!172\.(16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31))(?<!127)(?<!^10)(?<!^0)\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?<!192\.168)(?<!172\.(16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31))\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
-REGEX_IPV6 = r'(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
-REGEX_DOMAIN = r'[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 ip = Blueprint('ip',__name__)
 
@@ -14,7 +10,7 @@ def get_ip_information() :
     search_value = request.args.get('value')
     if search_value is None :
         abort(400)
-    if bool(re.fullmatch(REGEX_IPV4,search_value)) is False and bool(re.fullmatch(REGEX_IPV6,search_value)) is False and bool(re.fullmatch(REGEX_DOMAIN,search_value)) is False :
+    if regex_compare(search_value) is False :
         abort(400)
 
     information_json = get_ip(search_value)

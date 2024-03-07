e5108eb798e20ea54ca4b56c1af9d7c8a0bc2b7c
PrezidentB <biojoutd@gmail.com>
2024-03-07 15:45:26 +0100

    À propos: mise en forme

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,16 +1,33 @@
 <template>
+  <section class="container" style="padding-top: 13rem;">
+    <div class="title">À propos de nous</div>
+    <div class="sub-container">
+      <span class="subtitle">Bonjour !</span>
+      <p class="text">Bienvenue sur ce magnifique site ! 
+        Nous sommes trois étudiants du Pôle Sup de La Salle à Rennes et ceci est notre projet annuel. <br><br>
+        Nous sommes en Bachelor Sécurité Informatique (niveau BAC + 3) et avons donc eu l'idée de développer notre projet sur le thème de l'OSINT.
+        </p>
+    </div>
+    <div class="sub-container">
+      <div class="cards">
+        <div class="card">
+          <img src="./../assets/LE_NAIN.svg" alt="test" width="150px" height="150px" style="align-self: center; margin: auto;">
+        </div>
+        <div class="card">
+          <img src="./../assets/LE_NAIN.svg" alt="test" width="150px" height="150px" style="align-self: center; margin: auto;">
+        </div>
+        <div class="card">
+          <img src="./../assets/LE_NAIN.svg" alt="test" width="150px" height="150px" style="align-self: center; margin: auto;">
+        </div>
+        
+      </div>
+    </div>
+  </section>
   <section class="container">
-    <div>
-      <h1>Bienvenue sur ce début de about</h1>
-    </div>
-    <div class="sub-container2">
-      <span class="text">Le brief :</span>
-      <p> Vous en avez marre de toujours rechercher des outils d'OSINT à droite à gauche ? 
-        Vous vous perdez dans tous vos onglets ouverts ? pipOSINT est fait pour vous !<br><br>
-        Cette application vous procurera un gain de temps considérable lors de vos recherches.<br><br>
-        Pour votre plus grand bonheur nous avons rassemblé un maximum d'outils d'OSINT en un seul endroit, sous une seule interface. Et bonne nouvelle, 
-        il est aussi possible de porter cette version web dans un docker afin de self-host !</p>
-    </div>
+    <div class="title">En bref</div>
+  </section>
+  <section class="container">
+    <div class="title">Mentions Légales</div>
   </section>
 </template>  
 
@@ -19,9 +36,43 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-section {
-  padding-top: 13rem; // obligatoire pour le moment
-  padding-bottom: 5rem;
-  border-bottom: solid 1px rgba($color: #000000, $alpha: 0.1);
+@import url(./../assets/main.scss);
+
+.container {
+  display: flex;
+  flex-wrap: wrap-reverse;
+  flex-direction: row;
+  justify-content: center;
+  .title {
+    order: 1;
+    width: 50%;
+    padding-bottom: 1.5rem;
+    font-size: 3rem;
+    align-self: center;
+  }
+  .subtitle {
+    font-size: 2rem;
+  }
+  .sub-container {
+    display: flex;
+    width: 50%;
+    flex-direction: column;
+  }
+  .cards {
+    display: flex;
+    justify-content: space-between;
+    .card {
+      display: flex;
+      margin: 1rem;
+      width: 190px;
+      height: 254px;
+      border-radius: 30px;
+      background: #f7f9f4;
+      box-shadow: 15px 15px 30px #bebebe,
+                 -15px -15px 30px #ffffff;
+    }
+  
+  }
 }
+
 </style>

6e97e2cf4b3414c1c3ee78ebacabd3506760a12d
ArKoo <nico.arkoo@gmail.com>
2024-05-06 19:20:42 +0200

    Ajout du dark mode
    - Mise en fonction du switcher de theme
    - Maj du title et du favicon

--- frontend/app/src/components/Navbar.vue
+++ frontend/app/src/components/Navbar.vue
@@ -8,7 +8,7 @@
 
     <nav class="container">
       <a class="sides z-50 flex items-center gap-2 -ml-3 md:mr-12">
-          <img src="./../assets/LE_NAIN.svg" alt="pipOSINT">
+          <img src="./../assets/images/piposint.svg" alt="pipOSINT">
           <p>pipOSINT</p>
       </a>
       <ul v-show="!mobile" class="navigation">
@@ -17,7 +17,7 @@
         <li><a href="https://gitlab.com/bsi-dls/piposint" target="_blank">Gitlab</a></li>
         <li><router-link :to="{ name: 'Tutorials' }" >Tuto</router-link></li>
       </ul>
-      <i v-show="!mobile" class="fas fa-moon"></i>
+      <i v-show="!mobile" class="fas fa-moon" @click="toggleDarkMode"></i>
       <i v-show="mobile" @click="toggleMobileNavigation" class="fas fa-bars"></i>
       <transition name="mobile-nav">
         <ul v-show="mobileNav" class="mobile-navigation">
@@ -25,7 +25,7 @@
           <li><router-link :to="{ name: 'About' }" >À propos</router-link></li>
           <li><a href="https://gitlab.com/bsi-dls/piposint" target="_blank">Gitlab</a></li>
           <li><router-link :to="{ name: 'Tutorials' }" >Tuto</router-link></li>
-          <li><i class="fas fa-moon" style="padding: 0;"></i></li>
+          <li><i class="fas fa-moon" style="padding: 0;" @click="toggleDarkMode"></i></li>
         </ul>
       </transition>
     </nav>
@@ -37,36 +37,58 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export default {
-    name: 'Navbar',
-    data() {
-        return {
-            scrollPosition: null,
-            mobile: null,
-            mobileNav: null,
-            windowWidth: null,
-        };
+  name: 'Navbar',
+  data() {
+      return {
+          scrollPosition: null,
+          mobile: null,
+          mobileNav: null,
+          windowWidth: null,
+          colorTheme: "light_theme",
+      };
+  },
+  created () {
+    window.addEventListener('resize', this.checkScreen);
+    this.checkScreen();
+  },
+  methods: {
+    toggleMobileNavigation() {
+      this.mobileNav = !this.mobileNav;
     },
-    created () {
-      window.addEventListener('resize', this.checkScreen);
-      this.checkScreen();
+    toggleDarkMode() {
+      if (this.colorTheme === 'dark_theme') {
+        this.colorTheme = 'light_theme';
+        document.documentElement.classList.remove('dark_theme');
+      } else {
+        this.colorTheme = 'dark_theme';
+        document.documentElement.classList.remove('light_theme');
+      }
+      document.documentElement.classList.add(this.colorTheme);
+      localStorage.setItem('piposintTheme', this.colorTheme);
     },
-    methods: {
-      toggleMobileNavigation() {
-        this.mobileNav = !this.mobileNav;
-      },
-      checkScreen() {
-        this.windowWidth = window.innerWidth;
-        if (this.windowWidth <= 826) {
-          this.mobile = true;
-          return;
-        } else {
-          this.mobile = false;
-          this.mobileNav = false;
-          return;
-        }
-      },
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth <= 826) {
+        this.mobile = true;
+        return;
+      } else {
+        this.mobile = false;
+        this.mobileNav = false;
+        return;
+      }
     },
-    components: { FontAwesomeIcon }
+  },
+  mounted() {
+    if (localStorage.getItem('piposintTheme')) {
+      // Load the theme from local storage variable
+      this.colorTheme = localStorage.getItem('piposintTheme')
+    }
+    else {
+      localStorage.setItem('piposintTheme', this.colorTheme);
+    }
+    document.documentElement.classList.add(this.colorTheme);
+  },
+  components: { FontAwesomeIcon }
 };
 </script>
 
